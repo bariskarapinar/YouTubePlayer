09c57b05da16e087c976453fa2b1806ef241631b
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-25 15:05:25 +0300

    YouTube Widget Added

--- app/src/main/java/com/gamebit/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/gamebit/youtubeplayer/YoutubeActivity.kt
@@ -2,10 +2,14 @@ package com.gamebit.youtubeplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 import java.lang.reflect.Constructor
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
@@ -17,6 +21,16 @@ class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
 
         val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
         setContentView(layout)
+
+        /*val button1 = Button(this)
+        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
+        button1.text = "Button Added"
+        layout.addView(button1)*/
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
     }
 
     override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
