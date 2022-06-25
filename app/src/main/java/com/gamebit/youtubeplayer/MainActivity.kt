9e632183a6f1a90af7089f71f220b38e1272f4e0
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-25 20:55:07 +0300

    Implementing Intents

--- app/src/main/java/com/gamebit/youtubeplayer/MainActivity.kt
+++ app/src/main/java/com/gamebit/youtubeplayer/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.gamebit.youtubeplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPlaySingle.setOnClickListener(this)
+        btnStandalone.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val intent = when (view.id) {
+            R.id.btnPlaySingle -> Intent(this, YoutubeActivity::class.java)
+            R.id.btnStandalone -> Intent(this, StandaloneActivity::class.java)
+            else -> throw IllegalArgumentException("Undefined button clicked")
+        }
+        startActivity(intent)
     }
 }
